8025beefe39f94b82b068d2dc9f1e966f41299d5
Ebenezer Donkor <ebenezerdonkor890@gmail.com>
2025-09-10 11:38:11 +0000

    Update Footer.tsx

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full p-4 bg-gray-800 text-white text-center mt-auto">
+      <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
