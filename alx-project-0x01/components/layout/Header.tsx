5abf3446c7006abc8760d9b29d550fd7ba2181e1
Ebenezer Donkor <ebenezerdonkor890@gmail.com>
2025-09-10 11:37:39 +0000

    Update Header.tsx

---
+++ alx-project-0x01/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full p-4 bg-gray-800 text-white text-center">
+      <h1 className="text-2xl font-bold">My App Header</h1>
+    </header>
+  );
+};
+
+export default Header;
