88e772265762fe8a0174499d1d51ff517eec058c
Ebenezer Donkor <ebenezerdonkor890@gmail.com>
2025-09-10 11:52:53 +0000

    Update index.ts

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -1,6 +1,25 @@
-export interface PostProps {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+// interfaces/index.ts
+
+export interface UserProps {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: {
+      lat: string
+      lng: string
+    }
+  }
+  phone: string
+  website: string
+  company: {
+    name: string
+    catchPhrase: string
+    bs: string
+  }
 }
